7acc236943062f0d5055ca879596d8a22d9e2ea3
Shreyansh Diwakar <87513886+Shreyanshdiwakar@users.noreply.github.com>
2022-02-19 05:26:18 +0530

    Add files via upload

---
+++ AI-Chat-Bot/index.js
@@ -0,0 +1,46 @@
+
+const mySecret = process.env['Token ']
+require('dotenv')
+const { Client,Util, Collection,MessageEmbed,Structures } = require("discord.js");
+const keepAlive = require('./server.js')
+keepAlive()
+async function errorEmbed(text,message){
+      const newembed = new Discord.MessageEmbed()
+      .setColor("Random")
+      .setDescription(`**❌ | ${text} **`)
+       return message.channel.send(newembed);
+    }
+const Discord = require('discord.js');
+const client = new Client({
+    disableEveryone: true
+})
+const axios = require("axios")
+
+
+//=============================================
+
+const channel_id = "943865070297952332"
+
+
+//=============================================
+
+
+client.on('message', async (message) => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  try {
+  if (message.channel.id != channel_id) return
+  let res = await axios.get(`http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=1&msg=${encodeURIComponent(message.content)}`);
+  message.reply(res.data.cnt);
+  } catch {
+   errorEmbed(`Bot error, please try again!`,message)
+   }
+})
+client.on('ready', async () => {
+    console.clear()
+    console.log(`${client.user.tag} is online!`)
+})
+client.login(process.env.TOKEN);
+ 
+
+ 
